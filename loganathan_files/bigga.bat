ce7262f2e7a1447a6bdd934f5e05900783cf9dac
Shan10Code <jojogithub@gmail.com>
2023-09-25 00:10:48 +0530

    updated files

---
+++ loganathan_files/bigga.bat
@@ -0,0 +1,228 @@
+rename Infanion_Adobe_Phone_Gap_110.png          Adobe_Phone_Gap_110.png        
+
+rename Infanion_Ajax_110_0.png                   Ajax_110_0.png                 
+
+rename Infanion_Amazon_Aurora_110.png            Amazon_Aurora_110.png          
+
+rename Infanion_Amazon_AWS_110.png               Amazon_AWS_110.png             
+
+rename Infanion_Amazon_Dynamo_DB_110.png         Amazon_Dynamo_DB_110.png       
+
+rename Infanion_Android_110.png                  Android_110.png                
+
+rename Infanion_AngularJS_110_0.png              AngularJS_110_0.png            
+
+rename Infanion_Azure_110.png                    Azure_110.png                  
+
+rename Infanion_Bootstrap_110.png                Bootstrap_110.png              
+
+rename Infanion_Braintree_110.png                Braintree_110.png              
+
+rename Infanion_Browserstack_110.png             Browserstack_110.png           
+
+rename Infanion_C#_110_0.png                     C#_110_0.png                   
+
+rename Infanion_CK_Editor_110.png                CK_Editor_110.png              
+
+rename Infanion_Collabora_Office_110.png         Collabora_Office_110.png       
+
+rename Infanion_Connective_110.png               Connective_110.png             
+
+rename Infanion_Copernica_110.png                Copernica_110.png              
+
+rename Infanion_Craft_CMS_110.png                Craft_CMS_110.png              
+
+rename Infanion_Craft_commerce_110-v2.png        Craft_commerce_110-v2.png      
+
+rename Infanion_CSS3_110.png                     CSS3_110.png                   
+
+rename Infanion_cypress _110.png                 cypress _110.png               
+
+rename Infanion_Deep_Lens_V02_110.png            Deep_Lens_V02_110.png          
+
+rename Infanion_Dimona_110.png                   Dimona_110.png                 
+
+rename Infanion_Django_110.png_1.png             Django_110.png_1.png           
+
+rename Infanion_Docker_110.png                   Docker_110.png                 
+
+rename Infanion_Dot_Net_110_0.png                Dot_Net_110_0.png              
+
+rename Infanion_Drupal_110_1.png                 Drupal_110_1.png               
+
+rename Infanion_Drupal_commerce_110-v2.png       Drupal_commerce_110-v2.png     
+
+rename Infanion_e-contract_110.png               e-contract_110.png             
+
+rename Infanion_EDI_110.png                      EDI_110.png                    
+
+rename Infanion_Facebook_110.png                 Facebook_110.png               
+
+rename Infanion_Firebase_110.png                 Firebase_110.png               
+
+rename Infanion_Flutter_110.png                  Flutter_110.png                
+
+rename Infanion_Gitlab_110.png                   Gitlab_110.png                 
+
+rename Infanion_Google_110.png                   Google_110.png                 
+
+rename Infanion_Google_charts_110.png            Google_charts_110.png          
+
+rename Infanion_Google_maps_110.png              Google_maps_110.png            
+
+rename Infanion_Hibernate_110_1.png              Hibernate_110_1.png            
+
+rename Infanion_HTML5_110.png                    HTML5_110.png                  
+
+rename Infanion_Ingenico_110.png                 Ingenico_110.png               
+
+rename Infanion_Innosetup_110.png                Innosetup_110.png              
+
+rename Infanion_Instagram_110_0.png              Instagram_110_0.png            
+
+rename Infanion_Ionic_110.png                    Ionic_110.png                  
+
+rename Infanion_iOS_110.png                      iOS_110.png                    
+
+rename Infanion_itsme_110.png                    itsme_110.png                  
+
+rename Infanion_Java_110_0.png                   Java_110_0.png                 
+
+rename Infanion_Javascript_110.png               Javascript_110.png             
+
+rename Infanion_Jenkins_110.png                  Jenkins_110.png                
+
+rename Infanion_Jquery_110.png                   Jquery_110.png                 
+
+rename Infanion_Laraval_110_1.png                Laraval_110_1.png              
+
+rename Infanion_Lienion_110.png                  Lienion_110.png                
+
+rename Infanion_Ligatus_110.png                  Ligatus_110.png                
+
+rename Infanion_LinkedIn_110.png                 LinkedIn_110.png               
+
+rename Infanion_logo.png                         logo.png                       
+
+rename Infanion_Magento_110_0.png                Magento_110_0.png              
+
+rename Infanion_Manhattan_110.png                Manhattan_110.png              
+
+rename Infanion_Memcache_110.png                 Memcache_110.png               
+
+rename Infanion_Mercury_110.png                  Mercury_110.png                
+
+rename Infanion_Microsoft_ASP_NET_110.png        Microsoft_ASP_NET_110.png      
+
+rename Infanion_Microsoft_Dotnet_WPF_110.png     Microsoft_Dotnet_WPF_110.png   
+
+rename Infanion_Mollie-110.png                   Mollie-110.png                 
+
+rename Infanion_Mongo_DB_110.png                 Mongo_DB_110.png               
+
+rename Infanion_MyRent_110.png                   MyRent_110.png                 
+
+rename Infanion_MySQL_110.png                    MySQL_110.png                  
+
+rename Infanion_Nagios_110.png                   Nagios_110.png                 
+
+rename Infanion_Navision_110.png                 Navision_110.png               
+
+rename Infanion_Neoload_110.png                  Neoload_110.png                
+
+rename Infanion_New_Relic_110.png                New_Relic_110.png              
+
+rename Infanion_Node JS_110.png                  Node JS_110.png                
+
+rename Infanion_Office_110.png                   Office_110.png                 
+
+rename Infanion_OpenCV_110.png                   OpenCV_110.png                 
+
+rename Infanion_Oracle_110_0.png                 Oracle_110_0.png               
+
+rename Infanion_OWASP_ZAP_110.png                OWASP_ZAP_110.png              
+
+rename Infanion_pagerduty_110.png                pagerduty_110.png              
+
+rename Infanion_Pascal_110_0.png                 Pascal_110_0.png               
+
+rename Infanion_Payconiq_110.png                 Payconiq_110.png               
+
+rename Infanion_Paypal_110.png                   Paypal_110.png                 
+
+rename Infanion_PHP_110_0.png                    PHP_110_0.png                  
+
+rename Infanion_Postgre SQL_110.png              Postgre SQL_110.png            
+
+rename Infanion_pthread_110.png                  pthread_110.png                
+
+rename Infanion_Python_110_0.png                 Python_110_0.png               
+
+rename Infanion_Rabbit_MQ_110_0.png              Rabbit_MQ_110_0.png            
+
+rename Infanion_React_110.png                    React_110.png                  
+
+rename Infanion_React_Native_110.png             React_Native_110.png           
+
+rename Infanion_Rest_API_110.png                 Rest_API_110.png               
+
+rename Infanion_Ruby_on_Rails_110.png            Ruby_on_Rails_110.png          
+
+rename Infanion_SAML_110.png                     SAML_110.png                   
+
+rename Infanion_Selenium_110.png                 Selenium_110.png               
+
+rename Infanion_Shopify_110.png                  Shopify_110.png                
+
+rename Infanion_Simple_Weather_Gadget_110.png    Simple_Weather_Gadget_110.png  
+
+rename Infanion_Soap_110.png                     Soap_110.png                   
+
+rename Infanion_SoapUI_110.png                   SoapUI_110.png                 
+
+rename Infanion_Sonarqube_110.png                Sonarqube_110.png              
+
+rename Infanion_Spring_110_0.png                 Spring_110_0.png               
+
+rename Infanion_Spring_boot_110_0.png            Spring_boot_110_0.png          
+
+rename Infanion_Spring_MVC_110_0.png             Spring_MVC_110_0.png           
+
+rename Infanion_Spring_Tool_Suite_110.png        Spring_Tool_Suite_110.png      
+
+rename Infanion_SQlite_110.png                   SQlite_110.png                 
+
+rename Infanion_Supervisor_110.png               Supervisor_110.png             
+
+rename Infanion_SVG_110.png                      SVG_110.png                    
+
+rename Infanion_Symfony_110.png                  Symfony_110.png                
+
+rename Infanion_Tableau_110.png                  Tableau_110.png                
+
+rename Infanion_TeamViewer_110.png               TeamViewer_110.png             
+
+rename Infanion_Technology_Dart.png              Technology_Dart.png            
+
+rename Infanion_Technology_Express.png           Technology_Express.png         
+
+rename Infanion_Technology_Locust.png            Technology_Locust.png          
+
+rename Infanion_Twitter_110.png                  Twitter_110.png                
+
+rename Infanion_TypeScript_110.png               TypeScript_110.png             
+
+rename Infanion_UIPath_110.png                   UIPath_110.png                 
+
+rename Infanion_WebRTC_110.png                   WebRTC_110.png                 
+
+rename Infanion_Yii_110-v2.png                   Yii_110-v2.png                 
+
+rename Infanion_Youtube_110.png                  Youtube_110.png                
+
+rename Infanion_Zend_framework_110.png           Zend_framework_110.png         
+
+rename Infanion_Zendesk_110.png                  Zendesk_110.png                
+
+
+
